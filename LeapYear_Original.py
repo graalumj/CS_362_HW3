3f19155068946b9835b5fd8cfd0516e97ddddedb
GraalumJ <graalumj@oregonstate.edu>
2021-04-24 13:11:55 -0700

    Added LeapYear_Original.py
    Added the original leap year python program

---
+++ LeapYear_Original.py
@@ -0,0 +1,14 @@
+# CS 362 Homework 1
+# By: Jason Graalum
+# Run with "Python Jason_Graalum_hw1.py" and enter an integer year when prompted
+year = (int(input("Please enter year to check: ")))
+if (year % 4 == 0):
+    if (year % 100 == 0):
+        if (year % 400 == 0):
+            print(str(year) + " is a leap year")
+        else :
+            print(str(year) + " is not a leap year")
+    else :
+        print(str(year) + " is a leap year")
+else :
+    print(str(year) + " is not a leap year")
